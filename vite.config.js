7d94e57c97864a4ea98d26cabdf8d59c04ef98aa
Anne Blankert <anne.blankert@geodan.nl>
2024-04-11 23:29:14 +0200

    configure vite for build of separate components

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-// vite.config.js
 export default {
     base: '',
     root: './',
     build: {
-      outDir: 'dist'
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          component1: 'src/components/maps/web-map-leaflet.js',
+          component2: 'src/components/maps/web-map-openlayers.js',
+          component3: 'src/components/maps/web-map.js',
+          component4: 'src/components/maps/web-mapbox-gl.js',
+          component5: 'src/components/maps/web-maplibre-gl.js'
+        },
+        output: {
+          assetFileNames: `assets/[name].[ext]`
+        }
+      }
     }// other options...
-  }
+}
